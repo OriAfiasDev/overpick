0ebdb6b5d4a6c9dd8cb9fc3bc21c0afcbc8602c8
Ori Afias <oriafias@Oris-MacBook-Pro.local>
2023-03-21 13:59:23 +0200

    font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import localFont from 'next/font/local';
+
+const bigNoodle = localFont({ src: '../fonts/big_noodle_titling_oblique.ttf' });
+
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={bigNoodle.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
