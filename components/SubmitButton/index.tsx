472d38982e35d48e7e761c7fb2ce13932dcf35cc
Ori Afias <oriafias@Oris-MacBook-Pro.local>
2023-03-23 13:08:50 +0200

    button component

---
+++ components/SubmitButton/index.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+interface Props extends PropsWithChildren {
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+export const SubmitButton: React.FC<Props> = (props) => <Button {...props}>{props.children}</Button>;
+
+const Button = styled.span<{ disabled?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #df5e1d;
+  border: 1px solid #df5e1d;
+  margin: 20px;
+  color: #fff;
+  font-size: 24px;
+  height: 70px;
+  padding: 20px;
+  cursor: pointer;
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 0.87)};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  transition: opacity 0.2s ease-in-out;
+
+  &:hover {
+    ${({ disabled }) => !disabled && 'opacity: 1'};
+  }
+`;
